25010e9258b87f4212136f62e9b15a3c7b4cd384
77lucaw <77lucawalker@gmail.com>
2026-04-02 15:52:50 -0700

    Delete jw-remodel directory

--- jw-remodel/js/script.js
+++
@@ -1,9 +0,0 @@
-async function loadHome() {
-  const res = await fetch('content/home.json');
-  const data = await res.json();
-
-  document.getElementById('subtitle').textContent = data.subtitle;
-  document.getElementById('cta').textContent = data.cta;
-}
-
-loadHome();
