23efcc6a9a2e9fe1b9339f4be4990d07123f64e2
Luca Walker <77lucawalker@gmail.com>
2026-04-02 16:35:51 -0700

    y

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 async function loadHome() {
   const res = await fetch('content/home.json');
   const data = await res.json();
@@ -7,14 +6,4 @@ async function loadHome() {
   document.getElementById('cta').textContent = data.cta;
 }
 
-=======
-async function loadHome() {
-  const res = await fetch('content/home.json');
-  const data = await res.json();
-
-  document.getElementById('subtitle').textContent = data.subtitle;
-  document.getElementById('cta').textContent = data.cta;
-}
-
->>>>>>> 09b0247f1e7b2d9ca6c7b158159c8e46a01ba191
 loadHome();
